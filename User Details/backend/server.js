b382507b3248183ad3e354ad2cd4b1858dbbdabc
Vineet Jadhav <vineet290403@gmail.com>
2024-03-19 12:07:58 +0530

    Ebill and UserDetails

---
+++ User Details/backend/server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect("URL_MONGO_URL");
+const userSchema = new mongoose.Schema({
+  username: { type: String, unique: true, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  fisrtName: { type: String, required: true },
+  lastName: { type: String, required: true },
+});
+
+const User = mongoose.model("User", userSchema);
+
+// Routes for CRUD operations
+
+app.post("/api/users", async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.status(200).json(newUser);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.get("/api/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
